1a4737a4e42a0e9db36e97da8854d73a2d430ea2
Arigbede Abiodun <arigbedeabiodun@gmail.com>
2020-06-21 18:52:16 +0100

    project update

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+      <div class="header">
+          <h3>Where in the world?</h3>
+          <div class="switch">
+              <span>dark mode</span>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .container {
+        padding: 20px 10px;
+        /* background-color: #f3f3f3; */
+    }
+    .header {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
